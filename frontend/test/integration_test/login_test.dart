a5f78846817a36ea9d07a7bfb588c3fb27376e50
David B. Adrian <dawidh.adrian@googlemail.com>
2024-12-08 10:47:25 +0100

    Refactor initial integration/E2E test setup

--- frontend/test/integration_test/login_test.dart
+++ frontend/test/integration_test/login_test.dart
@@ -1,110 +1,22 @@
-import 'dart:io';
-
-import 'package:downloadsfolder/downloadsfolder.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:updat/utils/global_options.dart';
-import 'package:zest/config/constants.dart';
-import 'package:zest/settings/settings_provider.dart';
-import 'package:zest/main.dart';
-import 'package:integration_test/integration_test.dart';
+
+import 'shared_routines.dart';
 
 void main() async {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-// get rid of debugPrints
-  if (isProduction) {
-// analyser does not like empty function body
-// debugPrint = (String message, {int wrapWidth}) {};
-// so i changed it to this:
-    debugPrint = (String? message, {int? wrapWidth}) {};
-  }
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // set important variables
-  SharedPreferences.setMockInitialValues({
-    apiUrlKey: "http://localhost:1337/api/v1",
-  });
-
-  final sharedPrefs = await SharedPreferences.getInstance();
-
-  if (Platform.isWindows || Platform.isLinux) {
-// Initialize FFI
-    sqfliteFfiInit();
-  }
-
-  databaseFactory = databaseFactoryFfi;
-
-  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-      .setMockMethodCallHandler(
-          const MethodChannel('plugins.flutter.io/path_provider'),
-          (MethodCall methodCall) async {
-    return '.';
-  });
-
-  final appDocumentsDir = await getApplicationDocumentsDirectory();
-
-  final database = await openDatabase(
-// Set the path to the database. Note: Using the `join` function from the
-// `path` package is best practice to ensure the path is correctly
-// constructed for each platform.
-    join(appDocumentsDir.path, 'sqlite.db'),
-
-    onCreate: (db, version) {
-// Run the CREATE TABLE statement on the database.
-      return db.execute(
-        'CREATE TABLE $RECIPE_DRAFT_DB_KEY(id INTEGER PRIMARY KEY, updatedLast INT, state TEXT)',
-      );
-    },
-
-    onUpgrade: (Database db, int oldVersion, int newVersion) {
-      if (oldVersion < newVersion && newVersion == 3) {
-        db.execute(
-            "ALTER TABLE $RECIPE_DRAFT_DB_KEY ADD COLUMN updatedLast INT;");
-      }
-    },
-// Set the version. This executes the onCreate function and provides a
-// path to perform database upgrades and downgrades.
-    version: 3,
-  );
-
-  PackageInfo.setMockInitialValues(
-      appName: "abc",
-      packageName: "com.example.example",
-      version: "1.0",
-      buildNumber: "2",
-      buildSignature: "buildSignature");
-  packageInfo = await PackageInfo.fromPlatform();
-
-// Configure headers
-// TODO: Remove for public version
-  UpdatGlobalOptions.downloadReleaseHeaders = {
-    "Accept": "application/octet-stream",
-  };
-
-  FlutterSecureStorage.setMockInitialValues({});
+  var (sharedPrefs, database) = await prepareAppForIntegrationTest();
 
   group('end-to-end test', () {
     testWidgets('Check if main scaffold is rendered', (tester) async {
-      await tester.pumpWidget(ProviderScope(overrides: [
-        sharedPreferencesProvider.overrideWithValue(sharedPrefs),
-        sqliteDbProvider.overrideWithValue(database),
-      ], child: const ZestApp()));
-
+      await startAppDefault(tester,
+          sharedPrefs: sharedPrefs, database: database);
       expect(find.byKey(const Key('MainScaffold')), findsOneWidget);
     });
 
     testWidgets('Test login functionality via real API', (tester) async {
-      await tester.pumpWidget(ProviderScope(overrides: [
-        sharedPreferencesProvider.overrideWithValue(sharedPrefs),
-        sqliteDbProvider.overrideWithValue(database),
-      ], child: const ZestApp()));
+      await startAppDefault(tester,
+          sharedPrefs: sharedPrefs, database: database);
       final usernameKey = const Key('username');
       final passwordKey = const Key('password');
       expect(find.byKey(usernameKey), findsOneWidget);
@@ -145,21 +57,14 @@ void main() async {
             '{"pk":"...","username":"admin","email":"..","first_name":"...","last_name":"..."}'
       });
 
-      await tester.pumpWidget(ProviderScope(overrides: [
-        sharedPreferencesProvider.overrideWithValue(sharedPrefs),
-        sqliteDbProvider.overrideWithValue(database),
-      ], child: const ZestApp()));
-
+      await startAppDefault(tester,
+          sharedPrefs: sharedPrefs, database: database);
       final usernameKey = const Key('username');
       final passwordKey = const Key('password');
       expect(find.byKey(usernameKey), findsOneWidget);
       expect(find.byKey(passwordKey), findsOneWidget);
       final loginButton = find.byKey(const Key('login'));
       expect(loginButton, findsOneWidget);
-
-      // final fieldWidget = find.byKey(usernameKey).evaluate().first.widget;
-      // final fieldTextValue = (fieldWidget as TextFormField).controller!.text;
-      // print("YTOO >$fieldTextValue<");
 
       await tester.pumpAndSettle();
       final TextFormField formfield =
