2a7d28bdc19257ccd19859d27ef075982ae6a20b
David B. Adrian <dawidh.adrian@googlemail.com>
2025-01-01 23:52:43 +0100

    healthcekc for backend to ensure its running correctly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# start redis service
-sudo service redis-server start &
 
-# start postgres services
-sudo service postgresql start &
 
 # start nginx services
-sudo service nginx start &
+sudo service nginx start
 
-# Wait for a bit to ensure services have started
-sleep 2
+# start redis service
+sudo service redis-server start
+
+# IMPORTANT: CLEAN LAST AS HEALTH CHECK CHECKS FOR PORT 5432
+# start postgres services
+sudo service postgresql start
 
 # Print a message to indicate services are running
 echo "Services started. Opening Bash shell..."
