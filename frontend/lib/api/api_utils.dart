582ff5dbafb94ceeda43985cbb6ebf71df890c1d
David B. Adrian <dawidh.adrian@googlemail.com>
2025-01-03 16:44:08 +0100

    Add a service provider that checks the current network status

--- frontend/lib/api/api_utils.dart
+++ frontend/lib/api/api_utils.dart
@@ -1,9 +1,9 @@
 import 'package:zest/settings/settings_provider.dart';
 
 Uri getAPIUrl(SettingsState settings, String path,
-    {Map<String, dynamic>? queryParameters}) {
+    {Map<String, dynamic>? queryParameters, withPostSlash = true}) {
   final midSlash = path.startsWith('/') ? '' : '/';
-  final postSlash = path.endsWith('/') ? '' : '/';
+  final postSlash = path.endsWith('/') || !withPostSlash ? '' : '/';
   return Uri.parse('${settings.apiUrl}$midSlash$path$postSlash')
       .replace(queryParameters: queryParameters);
 }
