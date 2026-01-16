cc8f3d0fb1b3b206113dc9db446d607fe63b0188
David B. Adrian <dawidh.adrian@googlemail.com>
2026-01-16 10:22:52 +0100

    deactivate camera for now

--- frontend/lib/camera/screens.dart
+++ frontend/lib/camera/screens.dart
@@ -1,111 +1,111 @@
-// A widget that displays the picture taken by the user.
-import 'dart:io';
+// // A widget that displays the picture taken by the user.
+// import 'dart:io';
 
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
+// import 'package:camera/camera.dart';
+// import 'package:flutter/material.dart';
 
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
+// class DisplayPictureScreen extends StatelessWidget {
+//   final String imagePath;
 
-  const DisplayPictureScreen({super.key, required this.imagePath});
+//   const DisplayPictureScreen({super.key, required this.imagePath});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Display the Picture')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Display the Picture')),
+//       // The image is stored as a file on the device. Use the `Image.file`
+//       // constructor with the given path to display the image.
+//       body: Image.file(File(imagePath)),
+//     );
+//   }
+// }
 
-class TakePictureScreen extends StatefulWidget {
-  static String get routeName => 'recipe_camera';
-  static String get routeLocation => '/camera';
+// class TakePictureScreen extends StatefulWidget {
+//   static String get routeName => 'recipe_camera';
+//   static String get routeLocation => '/camera';
 
-  const TakePictureScreen({super.key});
+//   const TakePictureScreen({super.key});
 
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
+//   @override
+//   TakePictureScreenState createState() => TakePictureScreenState();
+// }
 
-class TakePictureScreenState extends State<TakePictureScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
+// class TakePictureScreenState extends State<TakePictureScreen> {
+//   late CameraController _controller;
+//   late Future<void> _initializeControllerFuture;
 
-  @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initializeCamera();
+//   }
 
-  Future<void> _initializeCamera() async {
-    try {
-      // Get the list of available cameras
-      final cameras = await availableCameras();
+//   Future<void> _initializeCamera() async {
+//     try {
+//       // Get the list of available cameras
+//       final cameras = await availableCameras();
 
-      // Prefer the back camera if available, otherwise fallback to first
-      final camera = cameras.firstWhere(
-        (cam) => cam.lensDirection == CameraLensDirection.back,
-        orElse: () => cameras.first,
-      );
+//       // Prefer the back camera if available, otherwise fallback to first
+//       final camera = cameras.firstWhere(
+//         (cam) => cam.lensDirection == CameraLensDirection.back,
+//         orElse: () => cameras.first,
+//       );
 
-      // Initialize the controller with the chosen camera
-      _controller = CameraController(camera, ResolutionPreset.medium);
+//       // Initialize the controller with the chosen camera
+//       _controller = CameraController(camera, ResolutionPreset.medium);
 
-      // Initialize the controller
-      _initializeControllerFuture = _controller.initialize();
+//       // Initialize the controller
+//       _initializeControllerFuture = _controller.initialize();
 
-      // Refresh the UI
-      if (mounted) setState(() {});
-    } catch (e) {
-      debugPrint('Error initializing camera: $e');
-    }
-  }
+//       // Refresh the UI
+//       if (mounted) setState(() {});
+//     } catch (e) {
+//       debugPrint('Error initializing camera: $e');
+//     }
+//   }
 
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     // Dispose of the controller when the widget is disposed.
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Photograph Recipe...')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            // await _initializeControllerFuture;
-            final image = await _controller.takePicture();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Photograph Recipe...')),
+//       body: FutureBuilder<void>(
+//         future: _initializeControllerFuture,
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             return CameraPreview(_controller);
+//           } else {
+//             return const Center(child: CircularProgressIndicator());
+//           }
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () async {
+//           try {
+//             // await _initializeControllerFuture;
+//             final image = await _controller.takePicture();
 
-            if (!context.mounted) return;
+//             if (!context.mounted) return;
 
-            await Navigator.of(context).push(
-              MaterialPageRoute<void>(
-                builder: (context) => DisplayPictureScreen(
-                  imagePath: image.path,
-                ),
-              ),
-            );
-          } catch (e) {
-            debugPrint('Error taking picture: $e');
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
-    );
-  }
-}
+//             await Navigator.of(context).push(
+//               MaterialPageRoute<void>(
+//                 builder: (context) => DisplayPictureScreen(
+//                   imagePath: image.path,
+//                 ),
+//               ),
+//             );
+//           } catch (e) {
+//             debugPrint('Error taking picture: $e');
+//           }
+//         },
+//         child: const Icon(Icons.camera_alt),
+//       ),
+//     );
+//   }
+// }
